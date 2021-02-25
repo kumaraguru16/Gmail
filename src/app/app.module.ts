6fa0ff9209e4692c9084630ae5de7bad6fb74b6c
kumaraguru16 <41797489+kumaraguru16@users.noreply.github.com>
2021-02-25 15:05:22 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
