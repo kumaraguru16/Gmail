84c990b6e5d2910445d4895b71b1fa21e6e8db3e
kumaraguru16 <41797489+kumaraguru16@users.noreply.github.com>
2021-02-25 15:05:45 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< Updated upstream
-  title = "title";
-=======
+
   title = 'prabu';
->>>>>>> Stashed changes
 }
