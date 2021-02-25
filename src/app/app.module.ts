ecb63c88f54bb6ef2740c237050d136fc66686ab
Ranjana08 <65497024+Ranjana08@users.noreply.github.com>
2021-02-25 16:56:51 +0530

    navbar
    navbar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { MainScreenComponent } from './main-screen/main-screen.component';
+=======
+<<<<<<< HEAD
+import { NavbarComponent } from './navbar/navbar.component';
+=======
+import { MainScreenComponent } from './main-screen/main-screen.component';
+>>>>>>> 84c990b6e5d2910445d4895b71b1fa21e6e8db3e
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     MainScreenComponent
+=======
+<<<<<<< HEAD
+    NavbarComponent
+=======
+    MainScreenComponent
+>>>>>>> 84c990b6e5d2910445d4895b71b1fa21e6e8db3e
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
