bec5f04a05afc86d6b351d69ecaca31be1743fef
prabusampath007 <79634402+prabusampath007@users.noreply.github.com>
2021-02-26 14:27:25 +0530

    a

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { NavbarService } from '../service/navbar.service';
 
 @Component({
@@ -6,14 +6,29 @@ import { NavbarService } from '../service/navbar.service';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent implements OnInit {
-  toggle: boolean = true;
+export class ContentComponent implements OnInit, OnChanges {
+
   constructor(private navbarservice: NavbarService) { }
 
-  ngOnInit(): void {
-    this.navbarservice.$toggle.subscribe(val => {
-      this.toggle = val;
+  shorterWidth = '950px';
+  largerWidth = '1135px';
+  @Input() isMenuButtonEnabled: boolean = true;
 
-    });
+
+  ngOnInit(){
+
+  }
+  ngOnChanges(){
+    if(this.isMenuButtonEnabled){
+      this.changeContainerWidth(this.shorterWidth);
+    }
+    else{
+      this.changeContainerWidth(this.largerWidth);
+    }
+  }
+
+  changeContainerWidth(width : string){
+    console.log("content width changed to..."+width);
+    document.getElementById("contentWidth")!.style.width = width;
   }
 }
--- src/app/leftnavbar/compose/compose.component.ts
+++ src/app/leftnavbar/compose/compose.component.ts
@@ -27,7 +27,6 @@ export class ComposeComponent implements OnInit, OnChanges {
   }
 
   changeContainerWidth(width : string){
-    console.log(width);
     document.getElementById("composeContainerWidth")!.style.width = width;
   }
 
