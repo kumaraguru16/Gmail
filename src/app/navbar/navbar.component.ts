6e22c60a5e309c335d73be412871da93e99b9ada
Ranjana08 <65497024+Ranjana08@users.noreply.github.com>
2021-02-28 11:19:28 +0530

    theme,left nav alt,right nav alt

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,13 +14,16 @@ export class NavbarComponent implements OnInit {
   black:string="#400000 ";
   white:string="white";
   //url:string="purple"
-  url:string="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWbN5CYyccoh94EBNB_7FuEmYi3wCexNQLvg&usqp=CAU')";
-
+  url1:string="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWbN5CYyccoh94EBNB_7FuEmYi3wCexNQLvg&usqp=CAU')";
+  url2:string="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNdatOUMHAXtdJjH5ZLobnmeIu6Szsf5-Rag&usqp=CAU')"
+  url3:string="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAQ06w_ypQkC-cu1O6xmoKhcV3uV0WNKoMZw&usqp=CAU')"
+  url4:string="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNdatOUMHAXtdJjH5ZLobnmeIu6Szsf5-Rag&usqp=CAU')"
   constructor(private navbarservice: NavbarService) { }
   
   ngOnInit(): void {
   }
   themevalue(value:string){
+    this.flag=!this.flag;
     console.log(value);
     this.theme.emit(value);
   }
